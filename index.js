439da56f51b54b920db2d7399b09814bee392219
Margaret142 <tabbywange75@gmail.com>
2024-11-09 03:29:44 +0300

    added skills and work experience

---
+++ index.js
@@ -0,0 +1,18 @@
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontents = document.getElementsByClassName("tab-contents");
+
+// function opentab(tabname, event) {
+//     // Loop through all tab links and remove "active-link" and "active-tab" classes
+//     for (let tablink of tablinks) {
+//         tablink.classList.remove("active-link");
+//     }
+//     for(let tabcontent of tabcontents) {
+//         tabcontent.classList.remove("active-tab");
+//     }
+
+//     // Add "active-link" to the clicked tab link
+//     event.currentTarget.classList.add("active-link");
+
+//     // Add "active-tab" to the content associated with the tabName
+//     document.getElementById(tabname).classList.add("active-tab");
+// }
